6a8d5a2d0a7c3d9af95f2cf446fb9a2a7e484260
KofiFloki <jeremiah.martey@yahoo.com>
2023-10-31 23:44:43 +0000

    Parametrize templates Task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,63 +1,37 @@
 #!/usr/bin/env python3
-""" This module instantiate Babel object in the app """
-from flask import Flask, render_template, request
+"""A Basic Flask app with internationalization support.
+"""
 from flask_babel import Babel
-app = Flask(__name__)
-babel = Babel(app)
+from flask import Flask, render_template, request
 
 
 class Config:
-    """ a class that configures the app """
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object(Config)
-
-
-@app.route('/', strict_slashes=False)
-def index():
-    """ returns 0-index.html page """
-    return render_template('3-index.html')
-
-
-@babel.localeselector
-def get_locale():
-    """ determines the best match for the user's languages """
-    return request.accept_languages.best_match(app.config.languages)
-
-
-if __name__ == "__main__":
-    app.run()#!/usr/bin/env python3
-""" This module instantiate Babel object in the app """
-from flask import Flask, render_template, request
-from flask_babel import Babel
 app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-class Config:
-    """ a class that configures the app """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-app.config.from_object(Config)
-
-
-@app.route('/', strict_slashes=False)
-def index():
-    """ returns 0-index.html page """
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
     return render_template('3-index.html')
 
 
-@babel.localeselector
-def get_locale():
-    """ determines the best match for the user's languages """
-    return request.accept_languages.best_match(app.config.languages)
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
